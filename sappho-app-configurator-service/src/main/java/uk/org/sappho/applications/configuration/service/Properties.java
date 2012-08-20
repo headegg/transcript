b3d680ab42fb44a854b3ccdf22d3795d6db3048f
Andrew Heald <andrew@heald.co.uk>
2012-08-20 18:26:19 +0100

    Clean up.

--- sappho-app-configurator-service/src/main/java/uk/org/sappho/applications/configuration/service/Properties.java
+++ sappho-app-configurator-service/src/main/java/uk/org/sappho/applications/configuration/service/Properties.java
@@ -1,28 +1,15 @@
 package uk.org.sappho.applications.configuration.service;
 
 import com.google.gson.Gson;
-import sun.security.krb5.Config;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.io.FilenameFilter;
 import java.util.HashMap;
 import java.util.Map;
 
 public class Properties extends WorkingCopy {
 
     private String jsonFilename;
-
-    class NameValue {
-
-        private Map<String, String> nameValuePairs;
-
-        public Map<String, String> getNameValuePairs() {
-
-            return nameValuePairs;
-        }
-    }
 
     public Properties(String environment, String application) throws ConfigurationException {
 
