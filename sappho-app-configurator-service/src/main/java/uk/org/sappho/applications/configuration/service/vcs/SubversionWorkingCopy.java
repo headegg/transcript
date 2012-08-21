93e4996c62f95414a2fcc65779445f7f114c9867
Andrew Heald <andrew@heald.co.uk>
2012-08-21 10:11:55 +0100

    Switch to SVNKit, which is easier to deploy and just as good as JavaHL in this context.

--- sappho-app-configurator-service/src/main/java/uk/org/sappho/applications/configuration/service/vcs/SubversionWorkingCopy.java
+++ sappho-app-configurator-service/src/main/java/uk/org/sappho/applications/configuration/service/vcs/SubversionWorkingCopy.java
@@ -1,21 +1,22 @@
 package uk.org.sappho.applications.configuration.service.vcs;
 
-import org.tigris.subversion.javahl.Depth;
-import org.tigris.subversion.javahl.Revision;
-import org.tigris.subversion.javahl.SVNClient;
+import org.tmatesoft.svn.core.SVNDepth;
+import org.tmatesoft.svn.core.wc.SVNClientManager;
+import org.tmatesoft.svn.core.wc.SVNRevision;
+import org.tmatesoft.svn.core.wc.SVNUpdateClient;
 import uk.org.sappho.applications.configuration.service.ConfigurationException;
 
 import java.io.File;
 
 public class SubversionWorkingCopy {
 
-    private static final SVNClient svnClient = new SVNClient();
+    private static final SVNUpdateClient svnUpdateClient = SVNClientManager.newInstance().getUpdateClient();
 
     public void update(String workingCopyPath, String name) throws ConfigurationException {
 
         try {
-            svnClient.update(new File(workingCopyPath, name).getCanonicalPath(),
-                    Revision.HEAD, Depth.infinity, false, false, false);
+            svnUpdateClient.doUpdate(new File[] { new File(workingCopyPath, name) },
+                    SVNRevision.HEAD, SVNDepth.INFINITY, false, false, false);
         } catch (Exception exception) {
             throw new ConfigurationException("Unable to update " + name + " from Subversion server", exception);
         }
