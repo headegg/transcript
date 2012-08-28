044dd297606a827cb007838ff9c6f1babaffee9e
Andrew Heald <andrew@heald.co.uk>
2012-08-28 15:46:31 +0100

    Add license terms.

---
+++ README.md
@@ -0,0 +1,3 @@
+This software is licensed under the GNU Affero General Public License, version 3.
+See [http://www.gnu.org/licenses/agpl.html](http://www.gnu.org/licenses/agpl.html) for full details of the license terms.
+Copyright 2012 Andrew Heald.
--- sappho-app-configurator-service/src/main/java/uk/org/sappho/applications/configuration/service/WorkingCopy.java
+++ sappho-app-configurator-service/src/main/java/uk/org/sappho/applications/configuration/service/WorkingCopy.java
@@ -1,3 +1,9 @@
+/**
+ *** This software is licensed under the GNU Affero General Public License, version 3.
+ *** See http://www.gnu.org/licenses/agpl.html for full details of the license terms.
+ *** Copyright 2012 Andrew Heald.
+ */
+
 package uk.org.sappho.applications.configuration.service;
 
 import uk.org.sappho.applications.configuration.service.vcs.SubversionWorkingCopy;
