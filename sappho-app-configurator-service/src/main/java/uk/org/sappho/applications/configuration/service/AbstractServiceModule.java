7259f1cb75eabc26141da5b0264a6e1fc53c8efa
Andrew Heald <andrew@heald.co.uk>
2012-09-10 12:06:53 +0100

    AC-3: Switch over to GPLv3 license.

--- sappho-app-configurator-service/src/main/java/uk/org/sappho/applications/configuration/service/AbstractServiceModule.java
+++ sappho-app-configurator-service/src/main/java/uk/org/sappho/applications/configuration/service/AbstractServiceModule.java
@@ -1,6 +1,6 @@
 /**
- *** This software is licensed under the GNU Affero General Public License, version 3.
- *** See http://www.gnu.org/licenses/agpl.html for full details of the license terms.
+ *** This software is licensed under the GNU General Public License, version 3.
+ *** See http://www.gnu.org/licenses/gpl.html for full details of the license terms.
  *** Copyright 2012 Andrew Heald.
  */
 
